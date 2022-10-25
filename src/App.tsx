fc9b80e6e5beac278f2facbfba7799121cbd064c
Landon Cassman <Landon.Cassman@gmail.com>
2022-10-24 20:52:21 -0400

    Adding Octodex react element

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import { Header } from './components/Header'
+import { Octocat } from './components/Octocat'
 
 export function App() {
   return (
     <div>
       <Header />
+      <Octocat
+        name="NUXtocat"
+        number="153"
+        link="https://octodex.github.com/nuxtocat/"
+        artistLink="https://github.com/cameronfoxly"
+      />
+      <Octocat />
+      <Octocat />
+      <Octocat />
+      <Octocat />
     </div>
   )
 }
--- src/components/Octocat.tsx
+++ src/components/Octocat.tsx
@@ -1,3 +1,46 @@
 import React from 'react'
 
-export function Octocat() {}
+type OctocatProps = {
+  name: string
+  number: string
+  link: string
+  artistLink: string
+}
+
+export function Octocat(props: OctocatProps) {
+  return (
+    <div className="OctoArticle">
+      <a className="OctocatImageLink" href={props.link}>
+        <img
+          className="OctoImage"
+          src="https://octodex.github.com//images/Terracottocat_Single.png"
+          width="290"
+          height="290"
+          alt="Terracottocat"
+        />
+      </a>
+
+      <div className="ArtistNamebox">
+        <ul>
+          <li id="Name">
+            #149:
+            <a href="https://octodex.github.com//terracottocat/">
+              <strong>Terracottocat</strong>
+            </a>
+          </li>
+          <li>
+            <a href="https://github.com/chubbmo">
+              <img
+                id="Picture"
+                src="https://github.com/chubbmo.png"
+                width="24px"
+                height="24px"
+                alt="chubbmo"
+              />
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  )
+}
