c9e27f4457639fd757e8ffc85ad07b0988ba22a9
Landon Cassman <Landon.Cassman@gmail.com>
2022-10-25 12:44:20 -0400

    Adding more Octocats

--- src/App.tsx
+++ src/App.tsx
@@ -6,15 +6,35 @@ export function App() {
   return (
     <div>
       <Header />
-      <Octocat
-        name="NUXtocat"
-        number="153"
-        link="https://octodex.github.com/nuxtocat/"
-        image="https://octodex.github.com/images/NUX_Octodex.gif"
-        artistLink="https://github.com/cameronfoxly"
-        artistAvatar="https://github.com/cameronfoxly.png"
-        artistName="Cameron Foxly"
-      />
+      <main>
+        <Octocat
+          name="NUXtocat"
+          number="153"
+          link="https://octodex.github.com/nuxtocat/"
+          image="https://octodex.github.com/images/NUX_Octodex.gif"
+          artistLink="https://github.com/cameronfoxly"
+          artistAvatar="https://github.com/cameronfoxly.png"
+          artistName="Cameron Foxly"
+        />
+        <Octocat
+          name="Saint Nictocat"
+          number="130"
+          link="https://octodex.github.com/saint_nictocat/"
+          image="https://octodex.github.com/images/saint_nictocat.jpg"
+          artistLink="https://github.com/heyhayhay"
+          artistAvatar="https://github.com/heyhayhay.png"
+          artistName="Haley Carroll"
+        />
+        <Octocat
+          name="Vinyltocat"
+          number="135"
+          link="https://octodex.github.com/vinyltocat/"
+          image="https://octodex.github.com/images/vinyltocat.png"
+          artistLink="https://github.com/suziejurado"
+          artistAvatar="https://github.com/suziejurado.png"
+          artistName="Suzie Jurado"
+        />
+      </main>
     </div>
   )
 }
